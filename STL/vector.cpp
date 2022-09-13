355f5c24f1c6ab804829d87359ab8b808fc103d4
Oleraj09 <olerajhossin@gmail.com>
2022-09-13 13:33:18 +0600

    STL Vector Added!

---
+++ STL/vector.cpp
@@ -0,0 +1,127 @@
+#include<bits/stdc++.h>
+bool desc(int a, int b){
+    return (a>b);
+}
+using namespace std;
+int main(){
+    cout<<"Vector STL all implemet!!"<<endl;
+    //declear any type of data inside vector
+    //vector<int>intvec;
+    //vector<long long>llvec;
+    //vector<char>charvec;
+    //vector<string>stringvec;
+
+
+    //vec.push_back() to store data in vector
+    //intvec.push_back(1);
+    //llvec.push_back(18192103072);
+    //charvec.push_back('A');
+    //stringvec.push_back("Oleraj Hossin");
+
+
+    //vec.size() to see the vector size
+    //cout<<intvec.size()<<endl;
+    //cout<<llvec.size()<<endl;
+    //cout<<charvec.size()<<endl;
+    //cout<<stringvec.size()<<endl;
+
+    //just print the vector with index number
+    //cout<<intvec[0]<<" ";
+    //cout<<llvec[0]<<" ";
+    //cout<<charvec[0]<<" ";
+    //cout<<stringvec[0]<<endl;
+    //cout<<today[0]<<" ";
+
+
+    //vector<long long>llv;
+    //llv.push_back(18192103072);
+    //llv.push_back(18192108012);
+    //llv.push_back(18192108045);
+    //we can replace index with another element
+    //llv[0]=18192103237;
+    //for(int i=0;i<llv.size();i++){
+    //    cout<<llv[i]<<" ";
+    //}
+
+
+    //int i;
+    //we can declear vector with specific number
+    //vector<int>v(5,2);
+    //v.push_back(11);
+    //for(i=0;i<v.size();i++){
+    //    cout<<v[i]<<" ";
+    //}
+
+
+    //Vector user input and output
+    //vector<int>v;
+    //int value,n,i;
+    //for(i=0;i<5;i++){
+    //    cin>>value;
+    //    v.push_back(value);
+    //}
+    //for(i=0;i<5;i++){
+    //    cout<<v[i]<<" ";
+    //}
+
+
+    //vector iterator function
+    //vector<int>v;
+    //vector<int>::iterator it;
+    //vector<string>v1;
+    //vector<string>::iterator it1;
+    //for(int i=0;i<=5;i++){
+    //    string s;
+    //    cin>>s;
+    //    v1.push_back(s);
+    //}
+    //for(int i=0;i<=5;i++){
+    //    cout<<v1[i]<<" ";
+    //}
+    //puts(" ");
+    //it = v.begin();
+    //it++;
+    //cout<<*it<<endl;
+
+
+    //vector<int>v;
+    //vector<int>::iterator it;
+    //for(int i=0;i<=5;i++){
+    //    v.push_back(i);
+    //}
+    //for(int i=0;i<=5;i++){
+    //    cout<<v[i]<<" ";
+    //}
+    //puts(" ");
+    //for(it=v.begin();it!=v.end();it++){
+    //    cout<<*it<<" ";
+    //}
+
+    //Charecter sorting using vector
+    //vector<int>::iterator it;
+    //vector<string>v1;
+    //vector<string>::iterator it1;
+    //for(int i=0;i<=5;i++){
+    //    string s;
+    //    cin>>s;
+    //    v1.push_back(s);
+    //}
+    //sort(v1.begin(),v1.end(),desc);
+    //for(int i=0;i<=5;i++){
+    //    cout<<v1[i]<<" ";
+    //}
+    //puts(" ");
+
+    //vector<int>v;
+    //vector<int>::iterator it;
+    //v.push_back(19);
+    //v.push_back(34);
+    //v.push_back(12);
+    //v.push_back(43);
+    //sort(v.begin(),v.end(),desc);
+    //sort(v.begin(),v.begin()+2);
+    //for(it=v.begin();it!=v.end();it++){
+    //    cout<<*it<<" ";
+    //}
+}
+
